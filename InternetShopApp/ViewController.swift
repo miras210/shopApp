8b5ba3755309f487a0250efd9f5af3966a88eae8
Miras <sektor.miras@gmail.com>
2021-02-02 04:16:29 -0800

    Initial Commit

---
+++ InternetShopApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternetShopApp
+//
+//  Created by Miras Alimov on 02.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
